20974e1c8fdb774ace91f46613db0a80e55977b9
Akshay-Jaisinghani <akshay.jaisinghani7@gmail.com>
2024-05-19 15:18:29 +0530

    Fresh Project!!!

---
+++ src/main/java/com/example/EmployeeAssessment/EmployeeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssessmentApplication.class, args);
+	}
+
+}
